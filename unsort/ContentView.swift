5d2dca170017bf18a01a51c9db92e5b21801f3ad
= <sasamotokazutoyo@gmail.com>
2026-02-07 13:30:32 +0900

    Initial Commit

---
+++ unsort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  unsort
+//
+//  Created by 笹本和豊 on 2026/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ unsort/unsortApp.swift
@@ -0,0 +1,17 @@
+//
+//  unsortApp.swift
+//  unsort
+//
+//  Created by 笹本和豊 on 2026/02/07.
+//
+
+import SwiftUI
+
+@main
+struct unsortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
